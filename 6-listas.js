45802b442a96d62dbb1138be457df9c31bbe2e73
FeSchneider7 <fernandomoraes26@gmail.com>
2025-01-10 14:39:34 -0300

    Create 6-listas.js

---
+++ 6-listas.js
@@ -0,0 +1,20 @@
+console.log ('Trabalhando com listas');
+
+// const salvador = 'Salvador';
+// const saoPaulo = 'São Paulo';
+// const rioDeJaneiro = 'Rio de Janeiro';
+
+const listaDeDestinos = new Array(
+    'Salvador',
+    'São Paulo',
+    'Rio de Janeiro',
+    'Curitiba',
+    )
+
+listaDeDestinos.push('Paris') // isso para adcionar itens na lista
+
+console.log ('Destinos Possiveis:');
+// console.log (salvador, saoPaulo, rioDeJaneiro);
+
+console.log (listaDeDestinos);
+
